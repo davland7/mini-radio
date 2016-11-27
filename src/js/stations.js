c97a7346db6b0b058ecc2441d9bec73bc10cde90
David Landry <davland7@gmail.com>
2016-11-27 15:05:12 -0500

    Update staions

--- src/js/stations.js
+++ src/js/stations.js
@@ -9,7 +9,7 @@ const stations = [
   {'title': 'ROCK 100.9', 'description': 'Québec', 'stream': 'http://icecast-chxx.rncm.ca/chxx.mp3', 'logo': 'chxx-fm.png'},
   {'title': 'Radio Classique', 'description': 'Montréal', 'stream': 'http://38.100.101.72/CJPXFMAAC_SC', 'logo': 'cjpx-fm.png'},
   {'title': 'Énergie 98.9', 'description': 'Québec', 'stream': 'http://15123.live.streamtheworld.com/CHIKFMAAC.aac', 'logo': 'chik-fm.png'},
-  {'title': '107,3 Rouge FM', 'description': 'Montréal', 'stream': 'http://6723.live.streamtheworld.com/CITEFMAAC.aac', 'logo': 'cite-fm.png'},
+  {'title': '107,3 Rouge FM', 'description': 'Montréal', 'stream': 'http://18153.live.streamtheworld.com/CITEFMAAC.aac', 'logo': 'cite-fm.png'},
   {'title': '91.9 Sport', 'description': 'Montréal', 'stream': 'http://icecast-cklx.rncm.ca/cklx.aac', 'logo': 'cklx-fm.png'},
   {'title': 'CHOM', 'description': 'Montréal', 'stream': 'http://17003.live.streamtheworld.com/CHOMFMAAC.aac', 'logo': 'chom-fm.png'},
   {'title': 'M FM 102,9FM', 'description': 'Québec', 'stream': 'http://mfm-lh.akamaihd.net/i/studioaudio_1@393667/master.m3u8', 'logo': 'cfom-fm.png'},
