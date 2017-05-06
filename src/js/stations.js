2bc2ff4da49820188ba24640725f4dbad8b66147
David Landry <davland7@gmail.com>
2017-05-06 10:18:36 -0400

    Chrome 58 bug

--- src/js/stations.js
+++ src/js/stations.js
@@ -1,17 +1,17 @@
 const stations = [
   {'title': '92.5 The Beat', 'description': 'Montréal', 'stream': 'http://beat-lh.akamaihd.net/i/studioaudio_1@393656/master.m3u8', 'logo': 'ckbe-fm.png'},
-  {'title': 'BLVD 102.1', 'description': 'Québec', 'stream': 'http://158.69.4.238:8000/cfel.mp3', 'logo': 'cfel-fm.png'},
+  {'title': 'BLVD 102.1', 'description': 'Québec', 'stream': 'http://78.46.192.120:8000/cfel.mp3', 'logo': 'cfel-fm.png'},
   {'title': 'CHOI 98,1 Radio X', 'description': 'Québec', 'stream': 'http://icecast-choi.rncm.ca/choi.aac', 'logo': 'choi-fm.png'},
   {'title': 'FM93', 'description': 'Québec', 'stream': 'http://fm93-lh.akamaihd.net/i/studioaudio_1@393666/master.m3u8', 'logo': 'cjmf-fm.png'},
   {'title': 'ICI Radio-Canada Première', 'description': 'Montréal', 'stream': 'http://2qmtl0.akacast.akamaistream.net/7/953/177387/v1/rc.akacast.akamaistream.net/2QMTL0', 'logo': 'cbft-fm.png'},
   {'title': '98,5FM', 'description': 'Montréal', 'stream': 'http://985-lh.akamaihd.net/i/studioaudio_1@393647/master.m3u8', 'logo': 'chmp-fm.png'},
   {'title': 'CKOI', 'description': 'Montréal', 'stream': 'http://ckoi-lh.akamaihd.net/i/studioaudio_1@393649/master.m3u8', 'logo': 'ckoi-fm.png'},
   {'title': 'Pop 100.9', 'description': 'Québec', 'stream': 'http://icecast-chxx.rncm.ca/chxx.mp3', 'logo': 'chxx-fm.png'},
-  {'title': 'Radio Classique', 'description': 'Montréal', 'stream': 'http://18843.live.streamtheworld.com/CJPXFMAAC.aac', 'logo': 'cjpx-fm.png'},
-  {'title': 'Énergie 98.9', 'description': 'Québec', 'stream': 'http://15123.live.streamtheworld.com/CHIKFMAAC.aac', 'logo': 'chik-fm.png'},
-  {'title': '107,3 Rouge FM', 'description': 'Montréal', 'stream': 'http://18153.live.streamtheworld.com/CITEFMAAC.aac', 'logo': 'cite-fm.png'},
+  {'title': 'Radio Classique', 'description': 'Montréal', 'stream': 'http://18863.live.streamtheworld.com/CJPXFMAAC.aac', 'logo': 'cjpx-fm.png'},
+  {'title': 'Énergie 98.9', 'description': 'Québec', 'stream': 'http://15093.live.streamtheworld.com/CHIKFMAAC.aac', 'logo': 'chik-fm.png'},
+  {'title': '107,3 Rouge FM', 'description': 'Montréal', 'stream': 'http://19293.live.streamtheworld.com/CITEFMAAC.aac', 'logo': 'cite-fm.png'},
   {'title': '91.9 Sport', 'description': 'Montréal', 'stream': 'http://icecast-cklx.rncm.ca/cklx.aac', 'logo': 'cklx-fm.png'},
-  {'title': 'CHOM', 'description': 'Montréal', 'stream': 'http://17003.live.streamtheworld.com/CHOMFMAAC.aac', 'logo': 'chom-fm.png'},
+  {'title': 'CHOM', 'description': 'Montréal', 'stream': 'http://16083.live.streamtheworld.com/CHOMFMAAC.aac', 'logo': 'chom-fm.png'},
   {'title': 'M FM 102,9FM', 'description': 'Québec', 'stream': 'http://mfm-lh.akamaihd.net/i/studioaudio_1@393667/master.m3u8', 'logo': 'cfom-fm.png'},
   {'title': 'Rythme FM', 'description': 'Montréal', 'stream': 'http://rythmemtl-lh.akamaihd.net/i/studioaudio_1@393689/master.m3u8', 'logo': 'cfgl-fm.png'}
 ];
