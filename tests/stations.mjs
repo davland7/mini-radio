b7de71f7378530a19c847de3e192550452bdb13c
davland7 <davland7@gmail.com>
2022-07-08 09:36:35 -0400

    Update test

--- tests/stations.mjs
+++ tests/stations.mjs
@@ -1,19 +1,18 @@
 import fetch from 'node-fetch';
 import chalk from 'chalk';
-import stations from '../src/stations.json' assert {type: "json"};
+import { createRequire } from "module";
 
+const require = createRequire(import.meta.url);
+const data = require('../src/stations.json');
 const error = chalk.red;
 const passed = chalk.green;
 
-stations.forEach((station, index) => {
+data.forEach((item, index) => {
   setTimeout(() => {
-    fetch(station.src)
-      .then(() => {
-        fetch(station.src)
-          .then((res) => {
-            const messsage = `${station.title} - ${station.description}: status ${res.statusText} ${res.status}`;
-            console.log(res.ok ? passed(messsage) : error(messsage));
-          });
+    fetch(item.src)
+      .then((res) => {
+        const messsage = `${item.title} - ${item.description}: status ${res.statusText} ${res.status}`;
+        console.log(res.ok ? passed(messsage) : error(messsage));
       });
   }, index * 1000);
 });
